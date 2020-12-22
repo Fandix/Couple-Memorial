4863ebcb8aa037137093f47a3972873eb6e2ae74
fandix huang <fandix@weiruideMacBook-Pro.local>
2020-12-22 20:35:17 +0800

    create dateCounter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateCounterComponent } from './toolkit/date-counter/date-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateCounterComponent
   ],
   imports: [
     BrowserModule,
